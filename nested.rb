939f2121b7cbb5c5620d54de94c0df190a8d7cb6
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 21:56:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+   programmer_hash[:yukihiro_matsumoto] => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+    }
 end
 
 def changing_alan
